3f99ecd6154041f65ab6c0e097a1a9e6dcddc65f
konno57 <126854839+konno57@users.noreply.github.com>
2023-03-18 07:16:16 +0000

    [Finish]4-12

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -11,9 +11,12 @@ class PostImagesController < ApplicationController
   end
 
   def index
+    @post_images = PostImage.all
   end
 
   def show
+    @post_image = PostImage.find(params[:id])
+    
   end
   
   
